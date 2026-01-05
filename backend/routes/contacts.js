2660767f4702d1767152fb3f88fb9fdaf81ede76
Reno-mathews <renomathews10pro@gmail.com>
2026-01-05 12:29:34 +0530

    Update

---
+++ backend/routes/contacts.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const Contact = require("../models/Contact");
+
+// GET all contacts
+router.get("/", async (req,res) => {
+    const contacts = await Contact.find();
+    res.json(contacts);
+});
+
+// POST new contact
+router.post("/", async (req,res) => {
+    const contact = new Contact(req.body);
+})
