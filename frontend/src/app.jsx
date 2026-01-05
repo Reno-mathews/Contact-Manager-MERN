2ea1bbb5b330e88680a7aab4772cabc23c05a662
Reno-mathews <renomathews10pro@gmail.com>
2026-01-05 15:21:52 +0530

    Update

---
+++ frontend/src/app.jsx
@@ -0,0 +1,8 @@
+import { useEffect, useState} from "react"
+import { getContacts, addContact, deleteContact } from "./services/api";
+
+function App() {
+    const [contacts, setContacts] = useState([]);
+
+    
+}
