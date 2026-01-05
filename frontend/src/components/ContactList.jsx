b845ebd3a38b93b6798f0a69119326f025e05432
Reno-mathews <renomathews10pro@gmail.com>
2026-01-05 17:38:52 +0530

    Update

---
+++ frontend/src/components/ContactList.jsx
@@ -0,0 +1,12 @@
+function ContactList({ contacts, onDelete }) {
+    return (
+        <ul>
+            {contacts.map(c => (
+                <li key={c._id}>
+                    {c.name} - {c.phone}
+                    <button onClick={() => onDelete(c._id)}></button>
+                </li>
+            ))}
+        </ul>
+    )
+}
